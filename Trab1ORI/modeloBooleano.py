c6e437ced8e439881e5d5d3392749128252cfe54
joaopespindola <joaopedro.espindola@outlook.com>
2023-10-04 21:29:12 -0300

    corrigindo a criacao do indice.txt

--- Trab1ORI/modeloBooleano.py
+++ Trab1ORI/modeloBooleano.py
@@ -2,18 +2,19 @@ import nltk
 import sys
 from nltk.corpus import stopwords
 from nltk.stem import RSLPStemmer
+
 nltk.download('stopwords')
 nltk.download('rslp')
-#carregando a lista de stopwords
+
+# Carregando a lista de stopwords
 stopwordsPt = set(stopwords.words('portuguese'))
 indiceInvertido = {}
 stemmer = RSLPStemmer()
 
 def dividirTextoEmPalavras(texto):
     texto = ' '.join(texto.split())
-    caracteresSeparadores = ".,...,!?" 
+    caracteresSeparadores = ".,...,!?"
     for separador in caracteresSeparadores:
-        # Realiza a substituição de todos os caracteres no texto que correspondem ao valor da variável separador por um espaço em branco.
         texto = texto.replace(separador, ' ')
     palavras = texto.lower().split()
     return palavras
@@ -26,28 +27,37 @@ try:
         with open('indice.txt', 'w') as arquivo:
             # Fazendo a leitura da base de dados
             with open(arquivo_base, 'r') as album:
-                i = 1
+                numeroFaixa = 1
                 for faixa in album:
-                    # Remove qualquer espaço em branco no início ou final da linha
                     faixa = faixa.strip()
                     with open(f'C:/Users/joaop/OneDrive - Universidade Federal de Uberlândia/É O PROGRAMAS/Faculdade/ORI/Trab1ORI/base_samba/{faixa}', 'r') as arquivoFaixa:
                         for letra in arquivoFaixa:
                             palavras = dividirTextoEmPalavras(letra)
                             for palavra in palavras:
-                                indiceInvertido[faixa] = i
-                            # verifica se não é uma stopword e então coloca na estrutura de indiceInvertido
-                                if palavra not in stopwordsPt:    
-                                        arquivo.write(stemmer.stem(palavra) + ': \n')
-                                        if palavra not in indiceInvertido:
-                                            indiceInvertido[palavra] = 1
+                                # Verifica se não é uma stopword
+                                if palavra not in stopwordsPt:
+                                    # Tira o radical
+                                    radical = stemmer.stem(palavra)
+                                    # Verifica se o radical já está no dicionário da faixa
+                                    if radical in indiceInvertido:
+                                        # Se já está, atualiza a contagem da faixa
+                                        if numeroFaixa in indiceInvertido[radical]:
+                                            indiceInvertido[radical][numeroFaixa] += 1
                                         else:
-                                            # Se a palavra já existir no dicionário, incrementa o valor
-                                            indiceInvertido[palavra] += 1
-                        i += 1
+                                            indiceInvertido[radical][numeroFaixa] = 1
+                                    else:
+                                        # Se não está, cria a entrada para o radical na faixa
+                                        indiceInvertido[radical] = {numeroFaixa: 1}
+                    numeroFaixa += 1
+            # Escreve o índice invertido no arquivo
+            for radical, faixas in indiceInvertido.items():
+                arquivo.write(f"{radical}: ")
+                for faixa, contagem in faixas.items():
+                    arquivo.write(f"{faixa},{contagem}, ")
+                arquivo.write("\n")
     print(indiceInvertido, end="\n")
-                    
+
 except FileNotFoundError as e:
     print(f"Erro para encontrar o arquivo: {e}")
 except Exception as e:
     print(f"Ocorreu um erro inesperado: {e}")
-
